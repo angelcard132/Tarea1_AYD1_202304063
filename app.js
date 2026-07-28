9613231f7caad60e6d762c54bef25b14a516cbd6
angelcard132 <3046263250115@ingenieria.usac.edu.gt>
2026-07-27 21:52:05 -0600

    Reemplazar canción por álbum favorito

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/", (req, res) => {
     res.json({
         nombre: "Angel Cardenas",
-        cancionFavorita: "Hollywood - Peso Pluma"
+        cancionFavorita: "Bocanada - Gustavo Cerati",
     });
 });
 
